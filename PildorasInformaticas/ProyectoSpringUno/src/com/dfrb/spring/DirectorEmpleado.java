78ddc227666d42b63cb920e8d5a9092a17e6db0d
Daniel Rondon <danfelrondon@gmail.com>
2023-03-20 18:40:35 -0400

    Commit 07: PildorasInformaticas: creacion de clase DirectorEmpleado que implementa la interface Empleados - Este ejemplo no implementa ninguna caracteristica de Spring

---
+++ PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/spring/DirectorEmpleado.java
@@ -0,0 +1,12 @@
+package com.dfrb.spring;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class DirectorEmpleado implements Empleados {
+    @Override
+    public String getTareas() {
+        return "Gestionar la Plantilla de la Empresa";
+    }
+}
