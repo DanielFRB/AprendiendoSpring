ef6f1bfafccda8872043c9d3e2154197e84acef5
Daniel Rondon <danfelrondon@gmail.com>
2023-03-20 18:50:13 -0400

    Commit 13: PildorasInformaticas: Demo ejemplo implementacion de Inversion of Control IOC - clase DirectorEmpleado que implementa Empleados

---
+++ PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/DirectorEmpleado.java
@@ -0,0 +1,12 @@
+package com.dfrb.ioc;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class DirectorEmpleado implements Empleados {
+    @Override
+    public String getTareas() {
+        return "Gestionar la Plantilla de la Empresa";
+    }
+}
