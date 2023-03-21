425b8f1e37d36aaa27a2304b0152089d91965905
Daniel Rondon <danfelrondon@gmail.com>
2023-03-20 21:23:38 -0400

    Commit 22: PildorasInformaticas: Inyeccion de Dependencias - Continuacion 2do Paso - se crea campo de clase tipo CreacionInformes, se crea Constructor que recibe param tipo CreacionInformes y se desarrolla metodo el getInforme()

--- PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/JefeEmpleado.java
+++ PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/JefeEmpleado.java
@@ -5,8 +5,21 @@ package com.dfrb.ioc;
  */
 
 public class JefeEmpleado implements Empleados {
+    // Creacion del Contructor que recibe un parametro  de tipo CreacionInforme donde se inyecta la dependencia (informeNuevo)
+    public JefeEmpleado(CreacionInformes informeNuevo) {
+        this.informeNuevo = informeNuevo;
+    }
+    
     @Override
     public String getTareas() {
         return "Gestiono a mis Empleados";
     }
+    
+    @Override
+    public String getInforme() {
+        return "Informe presentado por el Jefe con arreglos: "+ informeNuevo.getInforme(); // Se inyecta el metodo getInforme() de la Clase Informe
+    }
+    
+    // Creacion de campo tipo CreacionInforme (Interface)
+    private CreacionInformes informeNuevo;
 }
