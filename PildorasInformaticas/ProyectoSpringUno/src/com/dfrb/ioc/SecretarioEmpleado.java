c6ffd85ba79cfe5a7a96c7561fa447a923de7fa6
Daniel Rondon <danfelrondon@gmail.com>
2023-03-20 21:33:29 -0400

    Commit 23: PildorasInformaticas: Inyeccion de Dependencias - Continuacion 2do Paso - se crea campo de clase tipo CreacionInformes, se crea metodo Setter que recibe param tipo CreacionInformes y se desarrolla metodo el getInforme()

--- PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/SecretarioEmpleado.java
+++ PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/SecretarioEmpleado.java
@@ -5,8 +5,21 @@ package com.dfrb.ioc;
  */
 
 public class SecretarioEmpleado implements Empleados {
+    // Creacion de metodo Setter donde se realiza la Inyeccion de Dependencias
+    public void setInformeNuevo(CreacionInformes informeNuevo) {
+        this.informeNuevo = informeNuevo;
+    }
+    
     @Override
     public String getTareas() {
         return "Gestionar la Agenda de los Jefes";
     }
+    
+    @Override
+    public String getInforme() {
+        return "Informe generado por el Secretario: "+ informeNuevo.getInforme(); // Se inyecta el metodo getInforme() de la Clase Informe
+    }
+    
+    // Creacion de campo tipo CreacionInforme (Interface)
+    private CreacionInformes informeNuevo;
 }
