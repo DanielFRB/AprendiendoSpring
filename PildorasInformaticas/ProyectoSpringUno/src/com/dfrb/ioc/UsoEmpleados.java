28cc6e6eb12af80083a46dab2ef9c0b86a858a6b
Daniel Rondon <danfelrondon@gmail.com>
2023-03-21 11:02:51 -0400

    Commit 28: PildorasInformaticas - Inyeccion de Dependencias - Inyeccion de Campos de Clase - 3er Paso - cambiar el nombre de la instancia con que se crean los objetos a ser Inyectados (Empleados por DirectorEmpleado y/o SecretarioEmpleado

--- PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/UsoEmpleados.java
+++ PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/UsoEmpleados.java
@@ -11,15 +11,24 @@ public class UsoEmpleados {
     public static void main(String[] args) {
         // 1er Paso: Cargar el Contexto de la aplicacion (El archivo applicationContext.xml)
         ClassPathXmlApplicationContext miContexto = new ClassPathXmlApplicationContext("applicationContext.xml");
-        // 2do Paso: Pedir al archivo XML el Bean
+        
+        // 2do Paso: Pedir al archivo XML el Bean por medio de la Interface Empleados
         Empleados Oswaldo = miContexto.getBean("miDirectorEmp", Empleados.class);
         Empleados Yaneth = miContexto.getBean("miJefeEmp", Empleados.class);
-        Empleados Daniel = miContexto.getBean("miSecretarioEmp", Empleados.class);
+        
+        // Para poder tener acceso a las variables privadas de la Clase SecretarioEmpleado, es necesario cambiar
+        // la forma en que se llama al Bean miSecretarioEmpleado, cambiando la forma como se pide el Bean a Spring
+        // de Empleados a SecretarioEmpleado.
+        // Empleados Daniel = miContexto.getBean("miSecretarioEmp", Empleados.class);
+        SecretarioEmpleado Daniel = miContexto.getBean("miSecretarioEmp", SecretarioEmpleado.class);
+        SecretarioEmpleado Carlos = miContexto.getBean("miSecretarioEmp", SecretarioEmpleado.class);
+        DirectorEmpleado Barack = miContexto.getBean("miDirectorEmp", DirectorEmpleado.class);
         // 3er Paso: Imprimir los Objetos creados.
         System.out.println(Oswaldo.getTareas());
         System.out.println(Yaneth.getTareas());
         System.out.println(Daniel.getTareas());
         System.out.println("----------------------------------------");
+        
         // 4to Paso en la Inyeccion de Dependencias
         // El objeto Oswaldo utiliza la Inyeccion de Dependencias para ejecutar el metodo getInforme()
         System.out.println(Oswaldo.getInforme());
@@ -27,6 +36,17 @@ public class UsoEmpleados {
         System.out.println(Yaneth.getInforme());
         // El objeto Daniel utiliza la Inyeccion de Dependencias para ejecutar el metodo getInforme()
         System.out.println(Daniel.getInforme());
+        System.out.println("----------------------------------------");
+        System.out.println("Inyeccion de Campos de Dependencia a Objetos de tipo SecretarioEmpleado");
+        System.out.println("Objeto Daniel - Email: "+ Daniel.getEmail());
+        System.out.println("Objeto Daniel - Empresa: "+ Daniel.getNombreEmpresa());
+        System.out.println("Objeto Carlos - Email: "+ Carlos.getEmail());
+        System.out.println("Objeto Carlos - Empresa: "+ Carlos.getNombreEmpresa());
+        System.out.println("----------------------------------------");
+        System.out.println("Inyeccion de Campos de Dependencia a Objetos de tipo DirectorEmpleado");
+        System.out.println("Objeto Barack - Email: "+ Barack.getEmail());
+        System.out.println("Objeto Barack - Empresa: "+ Barack.getNombreEmpresa());
+        
         // 4to Paso: Cerrar el Contexto para liberar recursos.
         miContexto.close();
     }
