4512d8f80322800bbec7995dbb0eb17758246584
Daniel Rondon <danfelrondon@gmail.com>
2023-03-20 21:18:52 -0400

    Commit 20: PildorasInformaticas: Inyeccion de Dependencias - 2do Paso - agregando metodo getInforme() en interface Empleados

--- PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/Empleados.java
+++ PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/Empleados.java
@@ -6,4 +6,7 @@ package com.dfrb.ioc;
 
 public interface Empleados {
     public String getTareas();
+    // 2do Paso: Agregando metodo getInforme(). Esto origina que las clases que implelenten esta interface tengan que
+    // desarrollar este nuevo metodo.
+    public String getInforme();
 }
