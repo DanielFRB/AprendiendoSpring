d01047356b825e1c3e0910190f79e2e0e00c9812
Daniel Rondon <danfelrondon@gmail.com>
2023-03-20 20:41:53 -0400

    Commit 18: PildorasInformaticas: Inyeccion de Dependencias - 1er Paso - Creacion de la Interface CreacionInformes para la Dependencia

---
+++ PildorasInformaticas/ProyectoSpringUno/src/com/dfrb/ioc/CreacionInformes.java
@@ -0,0 +1,11 @@
+package com.dfrb.ioc;
+
+/**
+ * @author danfe
+ */
+
+// 1er Paso para la implemenacion de la Inyeccion de Dependencias
+// Creacion de la Interface para la Dependencia.
+public interface CreacionInformes {
+    public String getInforme();
+}
