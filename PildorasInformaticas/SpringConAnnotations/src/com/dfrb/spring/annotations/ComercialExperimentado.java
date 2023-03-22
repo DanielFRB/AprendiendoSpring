8dc6a0e5a7ca6727b4c5073b793a4279e1ea4764
Daniel Rondon <danfelrondon@gmail.com>
2023-03-22 18:38:24 -0400

    Commit 44: PildorasInformaticas - SpringConAnnotations - Inyeccion de Dependencias con Constructor y Java Annotations: creacion del Constructor para inyeccion en clase que lo solicite y configuracion de la dependencia con @Autowired

--- PildorasInformaticas/SpringConAnnotations/src/com/dfrb/spring/annotations/ComercialExperimentado.java
+++ PildorasInformaticas/SpringConAnnotations/src/com/dfrb/spring/annotations/ComercialExperimentado.java
@@ -1,5 +1,6 @@
 package com.dfrb.spring.annotations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,8 +8,16 @@ import org.springframework.stereotype.Component;
  */
 
 // Se crea la Java Annotations @Component y el contenedor Spring registra el Bean con el nombre de "ComercialExperimentado"
-@Component("ComercialExperimentado")
+// Por regla general la anotacion @Component no lleva un nombre explicito, en estos casos Spring registra el Bean con el
+// nombre de la Clase.
+
+@Component
 public class ComercialExperimentado implements Empleados {
+    @Autowired
+    public ComercialExperimentado(CreacionInformeFinanciero informeFinanciero) {
+        this.informeFinanciero = informeFinanciero;
+    }
+    
     @Override
     public String getTareas() {
         return "Vender, vender y vender mas!!!";
@@ -16,7 +25,9 @@ public class ComercialExperimentado implements Empleados {
 
     @Override
     public String getInformes() {
-        return "Informe generado por el comercial";
+        //return "Informe generado por el comercial";
+        return informeFinanciero.getInformeFinanciero();
     }
     
+    private CreacionInformeFinanciero informeFinanciero;
 }
