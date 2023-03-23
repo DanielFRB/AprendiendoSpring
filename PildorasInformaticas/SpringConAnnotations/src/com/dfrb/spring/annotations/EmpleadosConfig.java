702bf38385069763ca45ed1c7f563b873053ae22
Daniel Rondon <danfelrondon@gmail.com>
2023-03-22 20:59:17 -0400

    Commit 50: PildorasInformaticas - SpringConAnnotations - Uso de las Java Annotations @Configuration y @ComponentScan que nos permitirá prescindir del archivo XML de configuración, y configurar Spring con código Java

---
+++ PildorasInformaticas/SpringConAnnotations/src/com/dfrb/spring/annotations/EmpleadosConfig.java
@@ -0,0 +1,14 @@
+package com.dfrb.spring.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Configuration
+@ComponentScan("com.dfrb.spring.annotations")
+public class EmpleadosConfig {
+	
+}
