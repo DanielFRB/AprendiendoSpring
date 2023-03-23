560a57670d8ea20129135f1823a0206f16eb96fe
Daniel Rondon <danfelrondon@gmail.com>
2023-03-22 21:40:09 -0400

    Commit 54: PildorasInformaticas - SpringConAnnotations - Java Annotations @Bean: creacion de los Beans en el archivo Java de Configuracion

--- PildorasInformaticas/SpringConAnnotations/src/com/dfrb/spring/annotations/EmpleadosConfig.java
+++ PildorasInformaticas/SpringConAnnotations/src/com/dfrb/spring/annotations/EmpleadosConfig.java
@@ -1,5 +1,6 @@
 package com.dfrb.spring.annotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.dfrb.spring.annotations")
 public class EmpleadosConfig {
+	// Definir el Bean para InformeFinancieroDptocompras
+	@Bean
+	public CreacionInformeFinanciero informeFinancieroDptoCompras() { // Sera el Id del Bean inyectado
+		return new InformeFinancieroDptoCompras();
+	}
 	
+	// Defin ir el Bean para DirectorFinanciero e Inyectar la Dependencia
+	@Bean
+	public Empleados directorFinanciero() {
+		return new DirectorFinanciero(informeFinancieroDptoCompras());
+	}
 }
