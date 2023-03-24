20c5016bdafe2af3c15c1c6b55a0995266c78ba3
Daniel Rondon <danfelrondon@gmail.com>
2023-03-23 20:47:02 -0400

    Commit 75: AprendiendoSpring - SpringMVCEjemplo1 - MVC: Uso de la anotacion @RequestParam para sustituir a HttpServletRequest y obtener el parametro del formulario. Esta anotacion simplifica el codigo

--- PildorasInformaticas/SpringMVCEjemplo1/src/main/java/com/dfrb/spring/mvc/HolaAlumnosControlador.java
+++ PildorasInformaticas/SpringMVCEjemplo1/src/main/java/com/dfrb/spring/mvc/HolaAlumnosControlador.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author dfrb@ne
@@ -21,9 +22,17 @@ public class HolaAlumnosControlador {
 		return "HolaAlumnosSpring";
 	}
 	
-	@RequestMapping("/procesarFormulario2")
+	/* @RequestMapping("/procesarFormulario2")
 	public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
 		String nombre = request.getParameter("nombreAlumno");
+		nombre += " es el mejor alumno";
+		String mensajeFinal = "Quien es el mejor Alumno? "+ nombre;
+		modelo.addAttribute("mensajeClaro", mensajeFinal);
+		return "HolaAlumnosSpring";
+	}*/
+	
+	// Uso de @RequestParam para sustituir HttpServletRequest
+	public String otroProcesoFormulario(@RequestParam("nombreAlumno") String nombre, Model modelo) {
 		nombre += " es el mejor alumno";
 		String mensajeFinal = "Quien es el mejor Alumno? "+ nombre;
 		modelo.addAttribute("mensajeClaro", mensajeFinal);
