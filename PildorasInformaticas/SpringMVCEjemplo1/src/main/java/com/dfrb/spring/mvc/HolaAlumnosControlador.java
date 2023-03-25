c96bc2ffc68414b3bead94f8122935b2941e59e6
Daniel Rondon <danfelrondon@gmail.com>
2023-03-25 09:05:14 -0400

    Commit 77: AprendiendoSpring - SpringMVCEjemplo1 - MVC: actualizacion de Demo ejemplo de resolucion de conflictos de rutas con @RequestMapping

--- PildorasInformaticas/SpringMVCEjemplo1/src/main/java/com/dfrb/spring/mvc/HolaAlumnosControlador.java
+++ PildorasInformaticas/SpringMVCEjemplo1/src/main/java/com/dfrb/spring/mvc/HolaAlumnosControlador.java
@@ -1,6 +1,5 @@
 package com.dfrb.spring.mvc;
 
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
