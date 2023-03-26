f1d305a4cba594ce06076c098be4d3a0a384a6e7
Daniel Rondon <danfelrondon@gmail.com>
2023-03-26 18:25:57 -0400

    Commit 104: AprendiendoSpring - ProyectoHibernate - Asignacion de Clave Principal en Hibernate: Demo ejemplo ingreso de registro a BBDD y su lectura, utilizando Hibernate

--- PildorasInformaticas/ProyectoHibernate/src/com/dfrb/hibernate/PruebaGuardaCliente.java
+++ PildorasInformaticas/ProyectoHibernate/src/com/dfrb/hibernate/PruebaGuardaCliente.java
@@ -9,11 +9,21 @@ public class PruebaGuardaCliente {
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Clientes.class).buildSessionFactory();
 		Session session = factory.openSession();
 		try {
-			Clientes clt1 = new Clientes("Daniel", "Machado", "Guanare");
+			Clientes clt1 = new Clientes("Barack", "Rondon Machado", "Guanare");
 			session.beginTransaction();
 			session.save(clt1);
 			session.getTransaction().commit();
 			System.out.println("Registro insertado correctamente en BBDD");
+			
+			// Lectura de Registro
+			session.beginTransaction();
+			System.out.println("Lectura de registro con Id: "+ clt1.getId());
+			Clientes cltRecuperado = session.get(Clientes.class, clt1.getId());
+			System.out.println("Registro: "+ cltRecuperado.toString());
+			session.getTransaction().commit();
+			System.out.println("Terminado!!!");
+			
+			// Cierre de la Sesion para liberar recursos
 			session.close();
 		} finally {
 			factory.close();
