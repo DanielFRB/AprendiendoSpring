8863b19a1f730461da505d9f7090f011a55ff147
Daniel Rondon <danfelrondon@gmail.com>
2023-03-27 11:59:48 -0400

    Commit 114: AprendiendoSpring - ProyectoRelacionesHibernate - Relaciones Uno a Uno: creacion y configuracion por medio de las anotaciones de la Clase Cliente. Para configurar la relacion se utiliza la anotacion @OneToOne y @JoinColum sobre una propiedad (variable) de tipo DetalleCliente

---
+++ PildorasInformaticas/ProyectoRelacionesHibernate/src/com/dfrb/hibernate/Cliente.java
@@ -0,0 +1,84 @@
+package com.dfrb.hibernate;
+
+import javax.persistence.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+	public Cliente() {
+	}
+	
+	public Cliente(String nombres, String apellidos, String direccion) {
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.direccion = direccion;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public DetalleCliente getDetalleCliente() {
+		return detalleCliente;
+	}
+
+	public void setDetalleCliente(DetalleCliente detalleCliente) {
+		this.detalleCliente = detalleCliente;
+	}
+
+	@Override
+	public String toString() {
+		return "Clientes [id=" + id + ", nombres=" + nombres + ", apellidos=" + apellidos + ", direccion=" + direccion
+				+ "]";
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id")
+	private int id;
+	
+	@Column(name = "nombres")
+	private String nombres;
+	
+	@Column(name = "apellidos")
+	private String apellidos;
+	
+	@Column(name = "direccion")
+	private String direccion;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id")
+	private DetalleCliente detalleCliente;
+}
