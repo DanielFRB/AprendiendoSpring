1c2a915b52c2d1f025631fbbde4d68cbd413c2d8
Daniel Rondon <danfelrondon@gmail.com>
2023-03-27 12:23:12 -0400

    Commit 118: AprendiendoSpring - ProyectoRelacionesHibernate - Relaciones Uno a Uno (Bidireccional): Demo ejemplo de como funciona la relacion uno a uno bi-direccional en una operacion de Consulta

---
+++ PildorasInformaticas/ProyectoRelacionesHibernate/src/com/dfrb/hibernate/ObtenerCliente.java
@@ -0,0 +1,39 @@
+package com.dfrb.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class ObtenerCliente {
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Cliente.class)
+				.addAnnotatedClass(DetalleCliente.class)
+				.buildSessionFactory();
+
+		Session session = factory.openSession();
+		try {
+			// Se comienza con la transaccion
+			session.beginTransaction();
+
+			// Se obtienen los detalles del cliente
+			DetalleCliente detalleCliente = session.get(DetalleCliente.class, 1);
+			System.out.println(detalleCliente.toString());
+			
+			// Obtiene al cliente
+			System.out.println(detalleCliente.getCliente().toString());
+			
+			// Hacer el commit
+			session.getTransaction().commit();
+
+			// Cierre de la Sesion para liberar recursos
+			session.close();
+		} finally {
+			factory.close();
+		}
+	}
+}
