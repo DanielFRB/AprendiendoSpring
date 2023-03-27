492db15603acf42a85c2191e9c7cbf1fc222c2e4
Daniel Rondon <danfelrondon@gmail.com>
2023-03-27 12:01:54 -0400

    Commit 114: AprendiendoSpring - ProyectoRelacionesHibernate - Relaciones Uno a Uno: creacion y configuracion por medio de las anotaciones de la Clase DetalleCliente. En esta clase no se configura ningun campo en especial xq la relacion es uni-direccional (Cliente --> DetalleCliente)

---
+++ PildorasInformaticas/ProyectoRelacionesHibernate/src/com/dfrb/hibernate/DetalleCliente.java
@@ -0,0 +1,72 @@
+package com.dfrb.hibernate;
+
+import javax.persistence.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Entity
+@Table(name = "detalle_cliente")
+public class DetalleCliente {
+	public DetalleCliente() {
+	}
+	
+	public DetalleCliente(String web, String telefono, String comentarios) {
+		this.web = web;
+		this.telefono = telefono;
+		this.comentarios = comentarios;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+	
+	@Override
+	public String toString() {
+		return "DetalleCliente [id=" + id + ", web=" + web + ", telefono=" + telefono + ", comentarios=" + comentarios
+				+ "]";
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "web")
+	private String web;
+	
+	@Column(name = "telefono")
+	private String telefono;
+	
+	@Column(name = "comentarios")
+	private String comentarios;
+}
