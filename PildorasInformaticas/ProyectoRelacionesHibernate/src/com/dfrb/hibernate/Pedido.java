98c2fedbeb7c1aab413dbd007a3636b7585b8e7b
Daniel Rondon <danfelrondon@gmail.com>
2023-03-29 19:51:15 -0400

    Commit 120: AprendiendoSpring - ProyectoRelacionesHibernate - Relaciones Uno a Varios, creacion de la Clase (Tabla) Pedido, se realiza la configuracion por medio de las Java Annotations, se utiliza la anotacion @ManyToOne.

---
+++ PildorasInformaticas/ProyectoRelacionesHibernate/src/com/dfrb/hibernate/Pedido.java
@@ -0,0 +1,60 @@
+package com.dfrb.hibernate;
+
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+	public Pedido(Date fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFormaPago() {
+		return formaPago;
+	}
+
+	public void setFormaPago(String formaPago) {
+		this.formaPago = formaPago;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", fechaPago=" + fechaPago + ", formaPago=" + formaPago + "]";
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "fecha_pago")
+	private Date fechaPago;
+	
+	@Column(name = "forma_pago")
+	private String formaPago;
+	
+	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+}
