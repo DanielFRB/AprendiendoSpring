3008da1eb614e2f50343c633de62d2aae77d8cfa
Daniel Rondon <danfelrondon@gmail.com>
2023-04-01 17:32:03 -0400

    Commit 146: AprendiendoSpring - PildorasInformaticas - PruebaGestionCRUD: actualizacion del Controlador de la Aplicacion, permite obtener la informacion del DAO y entregarla al modelo que sera mostrada por el JSP lista_clientes.

--- PildorasInformaticas/PruebaGestionCRUD/src/main/java/com/dfrb/spring/controlador/Controlador.java
+++ PildorasInformaticas/PruebaGestionCRUD/src/main/java/com/dfrb/spring/controlador/Controlador.java
@@ -1,8 +1,14 @@
 package com.dfrb.spring.controlador;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dfrb.spring.dao.ClienteDAO;
+import com.dfrb.spring.entidad.Cliente;
 
 /**
  * @author dfrb@ne
@@ -11,8 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/cliente")
 public class Controlador {
-	@RequestMapping("/lista")
-	public String listaClientes(Model modelo) {
-		return "lista_clientes";
-	}
+    @RequestMapping("/lista")
+    public String listaClientes(Model modelo) {
+	// Obtener Clientes del DAO
+	List<Cliente> clientes = clienteDAO.getClientes();
+		
+	// Entregar los Clientes al Modelo
+	modelo.addAttribute("clientes", clientes);
+	return "lista_clientes";
+    }
+	
+    @Autowired
+    private ClienteDAO clienteDAO;
 }
