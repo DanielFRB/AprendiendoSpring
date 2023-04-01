f71bcc4969c88aa4808bedd7b9b9ce30ba423bea
Daniel Rondon <danfelrondon@gmail.com>
2023-04-01 17:25:35 -0400

    Commit 144: AprendiendoSpring - PildorasInformaticas - PruebaGestionCRUD: creacion del Data Access Object DAO de la Entidad Cliente

---
+++ PildorasInformaticas/PruebaGestionCRUD/src/main/java/com/dfrb/spring/dao/ClienteDAO.java
@@ -0,0 +1,12 @@
+package com.dfrb.spring.dao;
+
+import java.util.List;
+import com.dfrb.spring.entidad.Cliente;
+
+/**
+ * @author dfrb@ne
+ */
+
+public interface ClienteDAO {
+    public List<Cliente> getClientes();
+}
