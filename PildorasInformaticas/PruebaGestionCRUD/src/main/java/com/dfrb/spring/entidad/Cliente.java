2ae5dea14645eeb5321f6c8730c16e474fa9b7a2
Daniel Rondon <danfelrondon@gmail.com>
2023-04-02 17:03:58 -0400

    Commit 147: AprendiendoSpring - PildorasInformaticas - PruebaGestionCRUD: actualizacion de la Entidad Cliente.

--- PildorasInformaticas/PruebaGestionCRUD/src/main/java/com/dfrb/spring/entidad/Cliente.java
+++ PildorasInformaticas/PruebaGestionCRUD/src/main/java/com/dfrb/spring/entidad/Cliente.java
@@ -9,57 +9,57 @@ import javax.persistence.*;
 @Entity
 @Table(name = "cliente")
 public class Cliente {
-	public Cliente() {
-	}
+    public Cliente() {
+    }
 	
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+	return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+	this.id = id;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+	return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public String getApellido() {
-		return apellido;
-	}
+    public String getApellido() {
+        return apellido;
+    }
 
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+    public void setApellido(String apellido) {
+	this.apellido = apellido;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+	return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+	this.email = email;
+    }
 	
-	@Override
-	public String toString() {
-		return "Cliente [id="+ id +", nombre="+ nombre +", apellido="+ apellido +", email="+ email +"]";
-	}
+    @Override
+    public String toString() {
+	return "Cliente [id="+ id +", nombre="+ nombre +", apellido="+ apellido +", email="+ email +"]";
+    }
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "nombre")
+    private String nombre;
 	
-	@Column(name = "nombre")
-	private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
 	
-	@Column(name = "apellido")
-	private String apellido;
-	
-	@Column(name = "email")
-	private String email;
+    @Column(name = "email")
+    private String email;
 }
