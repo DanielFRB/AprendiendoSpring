f382e0c9b53f9ed9146a131f25c98b9dadb1618e
Daniel Rondon <danfelrondon@gmail.com>
2023-04-04 19:36:42 -0400

    Commit 161: AprendiendoSpring - PildorasInformaticas - ProyectoGestionAOP: archivo de implementacion que desarrolla el metodo insertaCliente().

---
+++ PildorasInformaticas/ProyectoGestionAOP/src/java/com/dfrb/spring/dao/ClienteDAOImplement.java
@@ -0,0 +1,14 @@
+package com.dfrb.spring.dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Component
+public class ClienteDAOImplement {
+    public void insertaCliente() {
+        System.out.println("Cliente agregado a la BBDD");
+    }
+}
