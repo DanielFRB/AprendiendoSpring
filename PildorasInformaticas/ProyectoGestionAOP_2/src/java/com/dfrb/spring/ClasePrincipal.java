1c12250c6e3d2648dde9ab6948517c5758ac056c
Daniel Rondon <danfelrondon@gmail.com>
2023-04-05 11:18:47 -0400

    Commit 178: AprendiendoSpring - PildorasInformaticas - ProyectoGestionAOP_2: Clase Principal. Continuacion de ProyectoGestionAOP. Ordenacion de Aspectos

---
+++ PildorasInformaticas/ProyectoGestionAOP_2/src/java/com/dfrb/spring/ClasePrincipal.java
@@ -0,0 +1,28 @@
+package com.dfrb.spring;
+
+import com.dfrb.spring.aop.Cliente;
+import com.dfrb.spring.aop.Configuracion;
+import com.dfrb.spring.dao.ClienteDAOImplement;
+import com.dfrb.spring.dao.ClienteVIPDAOImplement;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        // Leer la confoguracion de Spring
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Configuracion.class);
+        // Obtener el Bean del contenedor de Spring
+        Cliente clt = new Cliente();
+        ClienteDAOImplement cliente = context.getBean("clienteDAOImplement", ClienteDAOImplement.class);
+        ClienteVIPDAOImplement clientevip = context.getBean("clienteVIPDAOImplement", ClienteVIPDAOImplement.class);
+        // Llamar al metodo
+        cliente.insertaCliente(clt);
+        System.out.println("------------------------");
+        clientevip.insertaClienteVIP(); // Por medio del Pointcut se puede ejecutar el aspecto sobre este metodo tambien
+        // Cerrar el Contexto
+        context.close();
+    }
+}
