0d29f60b7e383e818debf85c5ceec1e269ec7dc2
Daniel Rondon <danfelrondon@gmail.com>
2023-04-06 12:34:14 -0400

    Commit 196: AprendiendoSpring - PildorasInformaticas - PrimeraAppMaven: Demo App java con maven.

---
+++ PildorasInformaticas/PrimeraAppMaven/src/main/java/com/dfrb/primeraappmaven/App.java
@@ -0,0 +1,11 @@
+package com.dfrb.primeraappmaven;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
