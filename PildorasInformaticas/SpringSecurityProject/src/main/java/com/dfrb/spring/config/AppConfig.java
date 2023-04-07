031a331a7615a8f2d7656d95fc21273113851c20
Daniel Rondon <danfelrondon@gmail.com>
2023-04-07 11:41:11 -0400

    Commit 207: AprendiendoSpring - PildorasInformaticas - SpringSecurityProject: creacion del archivo de configuracion de Spring, que nos permitira resolver las vistas de los recursos internos, configurando su ubicacion y su extension (archivos JSP).

---
+++ PildorasInformaticas/SpringSecurityProject/src/main/java/com/dfrb/spring/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.dfrb.spring.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.dfrb.spring")
+public class AppConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
