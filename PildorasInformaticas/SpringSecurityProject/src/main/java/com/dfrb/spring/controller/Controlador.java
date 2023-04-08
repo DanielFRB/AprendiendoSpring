8a9fae16c1a7b00de2a4a7400a23153241063e9d
Daniel Rondon <danfelrondon@gmail.com>
2023-04-08 12:42:03 -0400

    Commit 211: AprendiendoSpring - PildorasInformaticas - SpringSecurityProject: agregando el Controlador de la Aplicacion y configuracion del metodo showInicio().

---
+++ PildorasInformaticas/SpringSecurityProject/src/main/java/com/dfrb/spring/controller/Controlador.java
@@ -0,0 +1,16 @@
+package com.dfrb.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Controller
+public class Controlador {
+    @GetMapping("/")
+    public String showInicio() {
+        return "index";
+    }
+}
