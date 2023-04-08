cd7445619ffd2b3bd283dfc79aac683a3c5f1ac5
Daniel Rondon <danfelrondon@gmail.com>
2023-04-08 17:17:18 -0400

    Commit 212: AprendiendoSpring - PildorasInformaticas - SpringSecurityProject: agregando archivo de configuracion del inicializador de seguridad de la Aplicacion.

---
+++ PildorasInformaticas/SpringSecurityProject/src/main/java/com/dfrb/spring/config/AppSecurityInitializer.java
@@ -0,0 +1,11 @@
+package com.dfrb.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class AppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    
+}
