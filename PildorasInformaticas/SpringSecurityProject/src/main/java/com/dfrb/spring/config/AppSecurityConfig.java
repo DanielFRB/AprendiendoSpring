41aee51a67e2bcdff56ce5e02a0d5fa5c8b5962e
Daniel Rondon <danfelrondon@gmail.com>
2023-04-09 20:11:44 -0400

    Commit 235: AprendiendoSpring - PildorasInformaticas - SpringSecurityProject: actualizacion archivo de configuracion de seguridad de Spring, se inyecta el Bean securityDataSource(), se autentica para establecer la conexion con la BBDD.

--- PildorasInformaticas/SpringSecurityProject/src/main/java/com/dfrb/spring/config/AppSecurityConfig.java
+++ PildorasInformaticas/SpringSecurityProject/src/main/java/com/dfrb/spring/config/AppSecurityConfig.java
@@ -1,12 +1,12 @@
 package com.dfrb.spring.config;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 /**
  * @author dfrb@ne
@@ -17,14 +17,8 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Código momentáneo mientras creamos la BBDD
-        UserBuilder usuario = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(usuario.username("Daniel").password("1234").roles("USER"))
-                .withUser(usuario.username("Yaneth").password("4567").roles("USER", "ADMIN"))
-                .withUser(usuario.username("Oswaldo").password("7890").roles("USER"))
-                .withUser(usuario.username("Barack").password("0123").roles("USER", "ADMIN"));
-        
+        // Autenticacion de la Conexion con la BBDD por medio del metodo inyectado securityDataSource()
+        auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override
@@ -40,4 +34,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and().exceptionHandling().accessDeniedPage("/accessDenied"); // Configurando Página de error
     }
+    
+    @Autowired
+    private DataSource securityDataSource; // Se inyecta el metodo configurado en el AppConfig
 }
