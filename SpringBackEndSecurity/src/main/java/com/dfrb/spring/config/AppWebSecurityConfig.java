dfa476d23c7bd1ba14f5d1eeaabcef17edfbbdf0
Daniel Rondon <danfelrondon@gmail.com>
2023-04-13 11:33:32 -0400

    Commit 263: AprendiendoSpring - SpringBackEndSecurity: actualizacion de archivo de configuracion de seguridad de la Aplicacion, se crea un @Bean PasswordEncode para encriptar las claves de los usuarios, se utiliza la autenticacion InMemory con un nuevo usuario y clave y se configura la Autorizacion Basica de Http.

--- SpringBackEndSecurity/src/main/java/com/dfrb/spring/config/AppWebSecurityConfig.java
+++ SpringBackEndSecurity/src/main/java/com/dfrb/spring/config/AppWebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.dfrb.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author dfrb@ne
@@ -13,14 +16,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class AppWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    // Se crea un Bean que nos permita codificar la contraseña del nuevo usuario
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll(); // Permite el acceso a todos sin pasar por ningun formulario de acceso
+        // http.authorizeRequests().anyRequest().permitAll(); // Permite el acceso a todos sin pasar por ningun formulario de acceso
+        http.authorizeRequests().anyRequest().authenticated(); // Obliga a que el usuario sea autenticado
+        http.formLogin(); // Coloca el formulario de acceso al inicio de la aplicación
+        http.httpBasic(); // Configura Autorizacion Básica
     }
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        // Se implementa un usuario y una contraseña para poder ingresar a la aplicación
+        // Se hace uso del @Bean passwordEncoder() para encriptar la contraseña
+        auth.inMemoryAuthentication().withUser("Daniel").password(passwordEncoder().encode("12345")).authorities("USER", "ADMIN");
     }
-    
 }
