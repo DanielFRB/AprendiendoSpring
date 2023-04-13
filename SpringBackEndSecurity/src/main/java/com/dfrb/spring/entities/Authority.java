1fc9cfeecb8809fb70eb93a16d3ce6e621ce80af
Daniel Rondon <danfelrondon@gmail.com>
2023-04-13 17:13:47 -0400

    Commit 266: AprendiendoSpring - SpringBackEndSecurity: agregando Clase Authority, esta clase guardara la informacion del tipo de Rol asignado al Usuario.

---
+++ SpringBackEndSecurity/src/main/java/com/dfrb/spring/entities/Authority.java
@@ -0,0 +1,54 @@
+package com.dfrb.spring.entities;
+
+import javax.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Table(name = "AUTH_AUTHORITY")
+@Entity
+public class Authority implements GrantedAuthority {
+    // Métodos sobresctitos de la Interface GrantedAuthority
+    @Override
+    public String getAuthority() {
+        return roleCode;
+    }
+    
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+    
+    // Campos de Clase
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+    @Column(name = "ROLE_CODE")
+    private String roleCode;
+	
+    @Column(name = "ROLE_DESCRIPTION")
+    private String roleDescription;
+}
