cc091dddeea8f95aec13e0933ffa5a699fd452c1
Daniel Rondon <danfelrondon@gmail.com>
2023-04-19 20:39:21 -0400

    Commit 283: AprendiendoSpring - SpringBackEndScurity: actualizacion de la Clase de Configuracion de Seguridad de la Aplicacion, se agrega un @Bean de tipo AutenticationManager y se configura el manejo de sesion agregando el filtrado con JWT, el cual verificara el estado del JWT durante la sesion.

--- SpringBackEndSecurity/src/main/java/com/dfrb/spring/config/AppWebSecurityConfig.java
+++ SpringBackEndSecurity/src/main/java/com/dfrb/spring/config/AppWebSecurityConfig.java
@@ -4,12 +4,17 @@ import com.dfrb.spring.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author dfrb@ne
@@ -31,20 +36,44 @@ public class AppWebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin(); // Coloca el formulario de acceso al inicio de la aplicación
         http.httpBasic(); // Configura Autorizacion Básica */
         
-        http.authorizeRequests((request) -> request.antMatchers("h2-console/**").permitAll().anyRequest().authenticated()).httpBasic();
-        http.formLogin();
+        // http.authorizeRequests((request) -> request.antMatchers("h2-console/**").permitAll().anyRequest().authenticated()).httpBasic();
+        // http.formLogin();
+        
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint).and()
+                .authorizeHttpRequests((request) -> request.antMatchers("/h2-console/**", "/api/v1/auth/login").permitAll()
+						.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated())
+                .addFilterBefore(new JWTAuthenticationFilter(userServices, jwtTokenHelper), UsernamePasswordAuthenticationFilter.class);
         
         // Consola h2
-        http.csrf().disable().headers().frameOptions().disable();
+        http.csrf().disable().cors().and().headers().frameOptions().disable();
     }
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Autenticacion inMemory
+        // auth.inMemoryAuthentication().withUser("Daniel").password(passwordEncoder().encode("test@123"))
+	//			.authorities("USER", "ADMIN");
+        
         // Autenticacion por BBDD utilizando UserServices
         auth.userDetailsService(userServices).passwordEncoder(passwordEncoder());
     }
     
-    // Inyeccion del Servicio
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    
+    // Inyeccion del Servicio de Usuario
     @Autowired
     private UserServices userServices;
+    
+    // Inyeccion del Componente que nos permite manejar el Token JWT
+    @Autowired
+    private JWTTokenHelper jwtTokenHelper;
+    
+    // Inyeccion de la Interface que permite initiar el esquema de autenticacion
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;  
 }
