65df18e586f799f6ab38393c7b2d2346a9b671d9
Daniel Rondon <danfelrondon@gmail.com>
2023-05-03 11:31:17 -0400

    Commit 313: AprendiendoSpring - SpringSeurityWithJWT: agregando la Clase que permite filtrar el Token JWT en el proceso de autenticacion del usuario.

---
+++ SpringSecurityWithJWT/src/main/java/com/dfrb/springjwt/security/jwt/AuthJwtTokenFilter.java
@@ -0,0 +1,64 @@
+package com.dfrb.springjwt.security.jwt;
+
+import com.dfrb.springjwt.services.UserDetailsServiceImpl;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * @author dfrb@ne
+ */
+
+public class AuthJwtTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = getJwtToken(request);
+            
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUsernameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        } catch (UsernameNotFoundException e) {
+            logger.error("No se puede configurar la autenticación de Usuario: {}", e.getMessage());
+        }
+        filterChain.doFilter(request, response);
+    }
+    
+    private String getJwtToken(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
+    }
+    
+    
+    
+    @Autowired
+    private JwtUtils jwtUtils;
+    
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    
+    private static final Logger logger = LoggerFactory.getLogger(AuthJwtTokenFilter.class);
+}
