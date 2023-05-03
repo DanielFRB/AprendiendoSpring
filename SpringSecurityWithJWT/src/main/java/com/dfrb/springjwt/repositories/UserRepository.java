7e557b3ce6b3b5dee8eb7cc0bebbd79ceb8d10a9
Daniel Rondon <danfelrondon@gmail.com>
2023-05-03 10:40:01 -0400

    Commit 302: AprendiendoSpring - SpringSeurityWithJWT: agregando @Repository UserRepository, repositorio de usuarios con la declaracion de tres metodos: findByUsername, existsByUsername y existsByEmail.

---
+++ SpringSecurityWithJWT/src/main/java/com/dfrb/springjwt/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.dfrb.springjwt.repositories;
+
+import com.dfrb.springjwt.domains.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author dfrb@ne
+ */
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    
+    Boolean existsByUsername(String username);
+    
+    Boolean existsByEmail(String email);
+}
