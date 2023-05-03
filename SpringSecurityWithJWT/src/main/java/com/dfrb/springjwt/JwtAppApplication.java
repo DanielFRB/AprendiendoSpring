f0d21dd565d3e2e1a2f330a69a7a6635fedf6af6
Daniel Rondon <danfelrondon@gmail.com>
2023-05-03 10:22:50 -0400

    Commit 298: AprendiendoSpring - SpringSeurityWithJWT: agregando la Clase principal de la Aplicacion.

---
+++ SpringSecurityWithJWT/src/main/java/com/dfrb/springjwt/JwtAppApplication.java
@@ -0,0 +1,11 @@
+package com.dfrb.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAppApplication {
+    public static void main(String[] args) {
+	SpringApplication.run(JwtAppApplication.class, args);
+    }
+}
